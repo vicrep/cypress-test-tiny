1a64c5af82a6f77152e718a246c9e9288a6a13e1
Victor Repkow <victor.repkow@dialogue.co>
2020-08-26 14:05:43 -0400

    chore: add repro

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,12 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import "./commands"
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+Cypress.on("uncaught:exception", (err) => {
+  if (err.message === "An error that should be caught") {
+    return false
+  }
+})
